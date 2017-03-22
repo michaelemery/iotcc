1876490edfb7ad2c1fac7a2f9d124e0de57d7d74
Michael Emery <michael.emery@icloud.com>
2017-03-22 18:45:33 +1300

    added global params

---
+++ microhort_params.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -------------------------------------
+#
+# microhort_params.py
+# Micro Horticulture Project
+# global environment parameters
+#
+# Massey University
+# 158335 Project Group 8
+#
+# -------------------------------------
+
+# micro-environment internal temperature
+temperature = 0.0
+
+# micro-environment internal temperature
+humidity = 0.0
+
+# forecast (6 hour) ambient temperature
+temperatureForecast = 0.0
+
+# forecast (6 hour) ambient humidity
+humidityForecast = 0.0
+
+# soil ph level
+ph = 0.0
+
+# hours over minimum light threshold
+lightHours = 0.0
