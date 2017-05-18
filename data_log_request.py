62a83576e09ac255ab7192f1872a9fe7be0e8149
monkeyfont <juddkw@gmail.com>
2017-05-19 00:31:31 +1200

    connectionrefusederror added
    added a try catch block to deal with being unable to connect to the
    server

--- data_log_request.py
+++ data_log_request.py
@@ -15,26 +15,30 @@ def http_request(event_entry):
 
     # Create the socket object
     clientSocket = socket(AF_INET, SOCK_STREAM)
+    try:
+        # Connect to the server at the specified port
+        clientSocket.connect((serverName,serverPort))
 
-    # Connect to the server at the specified port
-    clientSocket.connect((serverName,serverPort))
+        request = "HEAD / HTTP/1.0\nHost:localhost\n\n"+str(message)
 
-    request = "HEAD / HTTP/1.0\nHost:localhost\n\n"+str(message)
+        ## Send the message
+        clientSocket.send(request.encode())
 
-    ## Send the message
-    clientSocket.send(request.encode())
-
-    ## Receive the reply
-    response = clientSocket.recv(1024)
-    #return response
-    # For print the response
-    print('From Server:', response)
-    clientSocket.close()
+        ## Receive the reply
+        response = clientSocket.recv(1024)
+        #return response
+        # For print the response
+        print('From Server:', response)
+        clientSocket.close()
+    except ConnectionRefusedError:
+        response = "408 request timed out"
     if '200' not in str(response):
-        print("unable to write event to database")
+
         event_backlog = open('eventLog_backlog.txt', 'a')
         event_backlog.write(message+"\n")
         event_backlog.close()
+        print("unable to write event to database, so has been logged locally")
+        return "the event was unable to be logged to the database, so has been logged locally"
     else:
         event_backlog = open('eventLog_backlog.txt','r')
         past_events = event_backlog.readlines()
@@ -60,16 +64,17 @@ def http_request(event_entry):
                     break
             event_backlog = open('eventLog_backlog.txt','w')
             event_backlog.writelines(past_events)
-    return "The event(s) has been logged successfully"
+        else:
+            return "The event has been logged successfully"
 
 
-# print ("starting request")
-# event_entry = {
-#         'event_dtg:': str(datetime.now()),
-#         'event_hub_id': 'id number 3',
-#         'event_profile_id': 'hub profile 3',
-#         'event_sensor_type_id': 'sensor 3',
-#         'event_state': 'event 3',
-#         'event_message': 'too hot'
-#     }
-# http_request(event_entry)
+print ("starting request")
+event_entry = {
+        'event_dtg:': str(datetime.now()),
+        'event_hub_id': 'id number 3',
+        'event_profile_id': 'hub profile 3',
+        'event_sensor_type_id': 'sensor 3',
+        'event_state': 'event 3',
+        'event_message': 'too hot'
+    }
+http_request(event_entry)
