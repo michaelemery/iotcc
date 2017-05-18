a0d7f6f361221be576ef73109c5bd1f84a55cd93
monkeyfont <juddkw@gmail.com>
2017-05-19 00:01:51 +1200

    conection missing data log written
    the first version of the data log is written, missing the correct
    information to connect to the server

---
+++ data_log_request.py
@@ -0,0 +1,75 @@
+from socket import *
+import json
+from datetime import datetime
+
+
+def http_request(event_entry):
+    message = json.dumps(event_entry)
+
+    print ("message is: "+message)
+    # Address for the server
+    serverName = 'localhost'
+
+    # Server's listening port
+    serverPort = 8080
+
+    # Create the socket object
+    clientSocket = socket(AF_INET, SOCK_STREAM)
+
+    # Connect to the server at the specified port
+    clientSocket.connect((serverName,serverPort))
+
+    request = "HEAD / HTTP/1.0\nHost:localhost\n\n"+str(message)
+
+    ## Send the message
+    clientSocket.send(request.encode())
+
+    ## Receive the reply
+    response = clientSocket.recv(1024)
+    #return response
+    # For print the response
+    print('From Server:', response)
+    clientSocket.close()
+    if '200' not in str(response):
+        print("unable to write event to database")
+        event_backlog = open('eventLog_backlog.txt', 'a')
+        event_backlog.write(message+"\n")
+        event_backlog.close()
+    else:
+        event_backlog = open('eventLog_backlog.txt','r')
+        past_events = event_backlog.readlines()
+        event_backlog.close()
+        if not past_events == "":
+            for event in past_events:
+                # Connect to the server at the specified port
+                clientSocket = socket(AF_INET, SOCK_STREAM)
+                clientSocket.connect((serverName,serverPort))
+                print('message is',event)
+                request = "HEAD / HTTP/1.0\nHost:localhost\n\n"+str(event)
+
+                ## Send the message
+                clientSocket.send(request.encode())
+                response = clientSocket.recv(1024)
+    #return response
+    # For print the response
+                print('From Server:', response)
+                clientSocket.close()
+                if '200' in str(response):
+                    past_events.remove(event)
+                else:
+                    break
+            event_backlog = open('eventLog_backlog.txt','w')
+            event_backlog.writelines(past_events)
+    return "The event(s) has been logged successfully"
+
+
+# print ("starting request")
+# event_entry = {
+#         'event_dtg:': str(datetime.now()),
+#         'event_hub_id': 'id number 3',
+#         'event_profile_id': 'hub profile 3',
+#         'event_sensor_type_id': 'sensor 3',
+#         'event_state': 'event 3',
+#         'event_message': 'too hot'
+#     }
+# http_request(event_entry)
